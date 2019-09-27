281b734c4ee519cdbda091a5ce0c1f94e9f912e0
yuni499 <banirakiki1103@gmail.com>
2019-09-27 13:49:43 +0900

    ジョイスティック操作

---
+++ GraduationProject/Assets/Name/Tanaka/Script/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //ジェイスティック
+    [SerializeField]
+    private Joystick m_joystick = null;
+    //速度
+    [SerializeField]
+    private float m_vel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dx = m_joystick.Horizontal;
+        float dy = m_joystick.Vertical;
+
+        transform.Translate(dx * m_vel, 0.0f, dy * m_vel);
+    }
+}
