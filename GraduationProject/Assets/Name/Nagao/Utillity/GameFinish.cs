dce183872eb8d0d27dfb26c9cb1da1dcb12d5a22
NagaoMasaki <karamiso499@gmail.com>
2019-09-27 13:40:07 +0900

    タイトルとリザルトシーンの画面上でクリックするとシーンが切り替わる

---
+++ GraduationProject/Assets/Name/Nagao/Utillity/GameFinish.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//======================================================================================= 
+//! @file       GameFinish
+//! @brief      Escapeキーを押したらゲーム終了する
+//! @author     長尾昌輝
+//! @date       2019/09/26
+//! @note       PCでゲーム終了させるように
+//=======================================================================================
+
+public class GameFinish : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Escapeキーを押したらゲーム終了
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            //終了処理
+            Application.Quit();
+        }
+    }
+
+
+}
