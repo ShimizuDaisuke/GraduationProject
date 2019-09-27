2ac7742636d7931cfc614bc810a24cb2e50263bf
yuni499 <banirakiki1103@gmail.com>
2019-09-27 15:21:32 +0900

    ゴールフラグの追加

---
+++ GraduationProject/Assets/Name/Tanaka/Script/GoolFlag.cs
@@ -0,0 +1,41 @@
+﻿//=======================================================================================
+//! @file   GoolFlag.cs
+//! @brief  ゴールの旗の処理
+//! @author 田中歩夢
+//! @date   9月27日
+//! @note   ない
+//=======================================================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//ゴールフラグのクラス
+public class GoolFlag : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 衝突判定（Enter）
+    /// </summary>
+    /// <param name="other">衝突したオブジェクト</param>
+    void OnCollisionEnter(Collision other)
+    {
+        //ゴールの旗に当たったら
+        if (other.gameObject.tag == "Player")
+        {
+            //リザルトシーンへ
+            SceneManager.LoadScene("Result");
+        }
+    }
+}
