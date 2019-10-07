f5d69364a7d47aee0ca6bf2e417bae8786b210c3
akihirohattori <hattorisan05@gmail.com>
2019-10-07 12:20:09 +0900

    HPゲージの途中経過

---
+++ GraduationProject/Assets/Name/Hattori/UI/Script/HPGaugeController.cs
@@ -0,0 +1,40 @@
+﻿//===============================================
+//! @file       HPGaugeController
+//! @brief      HPゲージの処理
+//! @author     服部晃大
+//! @date       10/7
+//! @note       sliderを使う方法はダメ
+//===============================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPGaugeController : MonoBehaviour
+{
+    //イメージをいじくるための変数
+    private RectTransform imageRectSize;
+    
+    // 元の画像サイズ
+    [SerializeField] private Vector2 baseSize;
+
+    // 現在の画像サイズ
+    private Vector2 nowSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 現在のサイズの初期化
+        nowSize = baseSize;
+
+        imageRectSize = GetComponent<RectTransform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        // プレイヤーの体力によって、画像サイズを変更する
+        imageRectSize.sizeDelta = nowSize;
+    }
+}
