006b0e4a90b42c468cbf3aea581e041cdc30316c
Hashimoto__Tomotake <tomo_10817@yahoo.co.jp>
2019-10-16 09:01:08 +0900

    イベント用のカメラ作成中

---
+++ GraduationProject/Assets/Name/Hashimoto/Script/Camera/State/CamraEvent/CameraEvent.cs
@@ -0,0 +1,32 @@
+﻿// -----------------------------------------------------------------------------------------
+//! @file       CameraEvent.cs
+//!
+//! @brief      イベント用のカメラの動き
+//!
+//! @author     橋本 奉武
+//!
+//! @date       2019.10.14
+// -----------------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using EventKind = EventDirector.EventKIND;
+
+public class CameraEvent : MonoBehaviour
+{
+    /// <summary>
+    /// イベント用のカメラの更新処理
+    /// </summary>
+    /// <param name="eventkind">イベントの種類</param>
+    public void EventUpdate(EventKind eventkind)
+    {
+        // イベントの種類によって、カメラの動きを変える
+        switch(eventkind)
+        {
+            
+        }
+    }
+
+}
