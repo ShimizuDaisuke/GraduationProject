95006785956d2712f35f5101649f8887474281aa
Hashimoto__Tomotake <tomo_10817@yahoo.co.jp>
2019-11-13 12:02:59 +0900

    プレイヤーが下へ落ちる処理　完了

--- GraduationProject/Assets/Name/Hashimoto/Script/Player/Gravity.cs
+++ GraduationProject/Assets/Name/Hashimoto/Script/Player/Gravity.cs
@@ -13,21 +13,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 型名を省略
+using EventKind = EventDirector.EventKIND;
+
 public class Gravity : MonoBehaviour
 {
+    // イベントの監督
+    [SerializeField]
+    private GameObject EventDirectorObj = default;
+    
     // 重力
     [SerializeField]
-    private float gravity = 9.8f;
+    private float gravity = 100.0f;
 
     // rigidbody 
-    private Rigidbody rigidbody;
+    private new Rigidbody rigidbody;
+
+    // スクリプト：イベント監督用のスクリプト
+    private EventDirector Script_EventDirector;
 
     void Start()
     {
         // このオブジェクトのrigidbodyを取得する
         rigidbody = GetComponent<Rigidbody>();
 
-        // rigidbodyにある重力を無効にする
+        // スクリプト：イベント監督用のスクリプト 取得
+        Script_EventDirector = EventDirectorObj.GetComponent<EventDirector>();
+
+        // rigidbodyによる重力を無効にする
         rigidbody.useGravity = false;
 
     }
@@ -37,10 +50,26 @@ public class Gravity : MonoBehaviour
     /// </summary>
     void FixedUpdate()
     {
-        // 重力
-        Vector3 gravity_vec3 = new Vector3(0.0f, -gravity, 0.0f);
+        // イベントが発生されていない場合
+        if(Script_EventDirector.IsEventKIND == EventKind.NONE)
+        {
+            // rigidbodyによる重力を無効にする
+            rigidbody.useGravity = false;
 
-        // 常に重力が働く
-        rigidbody.AddForce(gravity_vec3, ForceMode.Acceleration);
+            // 重力
+            Vector3 gravity_vec3 = new Vector3(0.0f, -gravity, 0.0f);
+
+            // 手動で常に重力が働くようにする
+            rigidbody.AddForce(gravity_vec3, ForceMode.Acceleration);
+
+        }
+        else
+        {
+            // rigidbodyによる重力を有効にして、自動に重力が働くようにする
+            rigidbody.useGravity = true;
+
+        }
+
+
     } 
 }
