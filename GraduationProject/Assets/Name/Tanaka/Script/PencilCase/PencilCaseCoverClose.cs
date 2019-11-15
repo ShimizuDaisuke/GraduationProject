f909793d012987aed8aa1db44abc5ff5f6fa768a
yuni499 <banirakiki1103@gmail.com>
2019-11-15 09:29:35 +0900

    なんか残ってた

--- GraduationProject/Assets/Name/Tanaka/Script/PencilCase/PencilCaseCoverClose.cs
+++ GraduationProject/Assets/Name/Tanaka/Script/PencilCase/PencilCaseCoverClose.cs
@@ -9,8 +9,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//筆箱のカバーを閉じるクラス
 public class PencilCaseCoverClose : MonoBehaviour
 {
+    //当たったらゴール座標に移動のクラス
+    [SerializeField]
+    private GoolPosMovePlayer m_goolPosMovePlayer = default;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +26,12 @@ public class PencilCaseCoverClose : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(m_goolPosMovePlayer.GoolINFlag)
+        {
+            transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z + 1,1.0f); 
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, 90.0f, 200.0f), 0.5f);
+        }
+
+
     }
 }
