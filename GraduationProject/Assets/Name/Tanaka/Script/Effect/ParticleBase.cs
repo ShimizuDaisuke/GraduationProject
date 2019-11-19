f2a9474838415b79577188d28a3c5dbc9bd2cb11
yuni499 <banirakiki1103@gmail.com>
2019-11-19 16:20:11 +0900

    エフェクトたくさんいれた気がしたけど入れてなかった

---
+++ GraduationProject/Assets/Name/Tanaka/Script/Effect/ParticleBase.cs
@@ -0,0 +1,44 @@
+﻿//=======================================================================================
+//! @file   ParticleBase.cs
+//! @brief  あとから追従するエフェクト処理
+//! @author 田中歩夢
+//! @date   11月01日
+//! @note   ない
+//=======================================================================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//追従するエフェクトクラス
+public class ParticleBase : MonoBehaviour
+{
+    void OnEnable()
+
+    {
+
+        StartCoroutine(ParticleWorking());
+
+    }
+
+    IEnumerator ParticleWorking()
+
+    {
+
+        var particle = GetComponent<ParticleSystem>();
+
+
+
+        yield return new WaitWhile(() => particle.IsAlive(true));
+
+
+
+        Destroy(gameObject);
+
+    }
+
+#if false
+
+    
+#endif
+}
