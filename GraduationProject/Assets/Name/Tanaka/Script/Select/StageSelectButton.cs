d879dce61818e6944b0372622aa7489302d9156e
yuni499 <banirakiki1103@gmail.com>
2019-11-28 10:39:50 +0900

    playerの属性を追加、SelectSceneの追加

---
+++ GraduationProject/Assets/Name/Tanaka/Script/Select/StageSelectButton.cs
@@ -0,0 +1,24 @@
+﻿//=======================================================================================
+//! @file   StageSelectButton.cs
+//! @brief  ステージ選択ボタンの処理
+//! @author 田中歩夢
+//! @date   11月28日
+//! @note   ない
+//=======================================================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//ステージ選択ボタンのクラス
+public class StageSelectButton : MonoBehaviour
+{
+    //次のシーンの文字列
+    [SerializeField]
+    private string SelectScene = default;
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene(SelectScene);
+    }
+}
