6ffcd59c1819cf0888689ed6e76d6131d6820b6c
yuni499 <banirakiki1103@gmail.com>
2020-01-14 13:19:08 +0900

    カッター修正

---
+++ GraduationProject/Assets/Name/Tanaka/Script/Cutter/CutterController.cs
@@ -0,0 +1,71 @@
+﻿//=======================================================================================
+//! @file   CutterController.cs
+//! @brief  カッターの動きの処理
+//! @author 田中歩夢
+//! @date   01月14日
+//! @note   ない
+//=======================================================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//カッターの動きの処理
+public class CutterController : MonoBehaviour
+{
+    //カッターが当たり判定クラス
+    [SerializeField]
+    private CutterHitFlag m_cutterHitFlag = default;
+
+    //カッターオブジェクト
+    [SerializeField]
+    private GameObject m_cutterObj = null;
+
+    //始点
+    [SerializeField]
+    private Transform m_startPos = null;
+
+    //終点
+    [SerializeField]
+    private Transform m_endPos = null;
+
+    //スタート地点についたかのフラグ
+    private bool m_startFlag = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(m_cutterHitFlag.HitFlag)
+        {
+            if(!m_startFlag)
+            {
+                float step = 1.0f * Time.deltaTime;
+
+                m_cutterObj.transform.position = Vector3.MoveTowards(m_cutterObj.transform.position, m_startPos.transform.position, step);
+
+            }
+            else
+            {
+                Debug.Log("dfas");
+            }
+
+
+
+            if (m_cutterObj.transform.position == m_startPos.transform.position)
+            {
+                m_startFlag = true;
+                Debug.Log("aaaaaa");
+            }
+        }
+
+
+        
+
+    }
+}
---
+++ GraduationProject/Assets/Name/Tanaka/Script/Cutter/CutterHitFlag.cs
@@ -0,0 +1,29 @@
+﻿//=======================================================================================
+//! @file   CutterHitFlag.cs
+//! @brief  カッターが当たった時の処理
+//! @author 田中歩夢
+//! @date   01月14日
+//! @note   ない
+//=======================================================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//カッターが当たった時の処理
+public class CutterHitFlag : MonoBehaviour
+{
+    private bool m_hitFlag = false;
+   
+
+    void OnCollisionEnter(Collision collider)
+    {
+        if (collider.transform.tag == "Cutter")
+        {
+            m_hitFlag = true;
+        }
+    }
+
+    //フラグの取得・設定
+    public bool HitFlag { get { return m_hitFlag; } set { m_hitFlag = value; } }
+
+}
