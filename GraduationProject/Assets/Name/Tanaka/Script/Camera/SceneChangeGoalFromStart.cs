4fadb50c2a7559c0886ba9acbc1f44b16e3b1e7a
yuni499 <banirakiki1103@gmail.com>
2020-01-21 11:48:45 +0900

    カメラ

---
+++ GraduationProject/Assets/Name/Tanaka/Script/Camera/SceneChangeGoalFromStart.cs
@@ -0,0 +1,34 @@
+﻿//=======================================================================================
+//! @file   EventCameraGoalFromStart.cs
+//! @brief  シーンが変わったときにスタートからゴールへのカメラ動きのフラグ？
+//! @author 田中歩夢
+//! @date   01月21日
+//! @note   ない
+//=======================================================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//シーンが変わったときにスタートからゴールへのカメラ動きのフラグ？
+public class SceneChangeGoalFromStart : MonoBehaviour
+{
+    //イベントDirectorオブジェクト
+    private GameObject m_eventDirectorObj = null;
+    //イベントDirector
+    private EventDirector m_eventDirector = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        m_eventDirectorObj = GameObject.Find("Director/EventDirector");
+        m_eventDirector = m_eventDirectorObj.GetComponent<EventDirector>();
+
+        m_eventDirector.IsEventKIND = EventDirector.EventKIND.CAMERA_START_FROM_GOAL;
+    }
+}
---
+++ GraduationProject/Assets/Name/Tanaka/Script/EventCamera/EventCameraGoalFromStart.cs
@@ -0,0 +1,46 @@
+﻿//=======================================================================================
+//! @file   EventCameraGoalFromStart.cs
+//! @brief  イベントカメラ　スタートからゴールの動きの処理
+//! @author 田中歩夢
+//! @date   01月21日
+//! @note   ない
+//=======================================================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//イベントカメラ　スタートからゴールの動きの処理
+public class EventCameraGoalFromStart : CameraEventBase
+{
+    //[SerializeField]
+    //private m_cameraStartPos =default;
+
+
+    void Awake()
+    {
+        // 初期化する
+        Initilaize();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public override void MoveCameraByEvent()
+    {
+
+        //Camera3D.transform.position = Vector3.MoveTowards(Camera3D.transform.position, m_cameraMovePos.transform.position, m_cameraMoveSpeed);
+
+        ////目標角度まで回転する
+        //if (Camera3D.transform.localEulerAngles.x <= m_targetAngle)
+        //{
+        //    Camera3D.transform.RotateAround(Player.transform.position, Vector3.forward, -m_angleSpeed);
+        //}
+        //else
+        //{
+        //    m_moveFlag = true;
+        //}
+    }
+}
