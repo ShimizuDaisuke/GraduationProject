c04dd689f72f629f3923ec30a04f40bfe90a05d7
Hashimoto__Tomotake <tomo_10817@yahoo.co.jp>
2020-01-30 12:09:08 +0900

    commit

---
+++ GraduationProject/Assets/Name/Hashimoto/Script/UI/SelectTitle/SelectTitle.cs
@@ -0,0 +1,35 @@
+﻿//=======================================================================================
+//! @file   SelectTitle.cs
+//!
+//! @brief  セレクトシーンのタイトル名を上下に動かす
+//!
+//! @author 橋本奉武
+//!
+//! @date   10月10日
+//!
+//=======================================================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectTitle : MonoBehaviour
+{
+    // 
+
+
+   /// <summary>
+   /// 開始処理
+   /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    void Update()
+    {
+        
+    }
+}
